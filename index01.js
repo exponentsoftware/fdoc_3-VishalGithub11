6f8e0fce1f7f877853f633191501599f868e340e
Vishal Modanwal <modanwalvishal@gmail.com>
2021-08-01 22:25:39 +0530

    ArrayToObjects

--- index01.js
+++ index01.js
@@ -1,15 +1,15 @@
 // A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destruction the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable.
 // ```js
-// const student = ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]];
+const student = ["David", ["HTM", "CSS", "JS", "React"], [98, 85, 90, 95]];
 
-// const names = student[0];
-// const skills = student[1];
-// const scores = student[2];
-// const jsScore = student[2][3];
-// const reactScore = student[2][3];
+const names = student[0];
+const skills = student[1];
+const scores = student[2];
+const jsScore = student[2][3];
+const reactScore = student[2][3];
 
-// console.log(names, skills, scores);
-// console.log(jsScore, reactScore);
+console.log(names, skills, scores);
+console.log(jsScore, reactScore);
 
 ///////////////////Write a function called convertArrayToObject which can convert the array to a structured object.////
 
